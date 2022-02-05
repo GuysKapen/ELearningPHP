b8fe5acded616ee9a88eef0f18421afb488f6171
GuysKapen <guyskapen@gmail.com>
2022-02-04 15:05:59 -0900

    Add categories section

---
+++ inc/category.php
@@ -0,0 +1,48 @@
+<div id="home-cat">
+    <h3>Categories</h3>
+    <ul>
+        <li>
+            <a href="">
+                <i class="fas fa-code"></i>
+                <h4>Web development</h4>
+                <p>82</p>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-pencil-alt"></i>
+                <h4>Design</h4>
+                <p>8</p>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-headphones-alt"></i>
+                <h4>Music</h4>
+                <p>24</p>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-users"></i>
+                <h4>Bussiness</h4>
+                <p>32</p>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-globe"></i>
+                <h4>Marketing</h4>
+                <p>82</p>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <i class="fas fa-globe"></i>
+                <h4>Marketing</h4>
+                <p>82</p>
+            </a>
+        </li>
+    </ul>
+</div>
--- index.php
+++ index.php
@@ -14,6 +14,8 @@
 
     <?php include("inc/header.php") ?>
     <?php include("inc/slider.php") ?>
+    <?php include("inc/category.php") ?>
+
 </body>
 
 </html>
