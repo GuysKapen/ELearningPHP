caa1b516c960eae3adb7e2cf0e9fb10137d4dfb1
GuysKapen <guyskapen@gmail.com>
2022-02-22 16:21:57 -0900

    Design language admin page

--- admin/inc/edit_language.php
+++ admin/inc/edit_language.php
@@ -1,13 +1,27 @@
-<div id="body-right">
-    <h3>Edit category</h3>
-    <form id="edit-form" method="post" enctype="multipart/form-data">
-        <?php
-        $lang = get_language($_GET['edit_lang']);
-        echo '<input type="text" name="name" value="'. $lang["name"] .'" placeholder="Enter language name">';
-        echo '<input type="hidden" name="id" value="' . $_GET['edit_lang'] . '"></input>';
-        ?>
-        <button name="update_lang">Update language</button>
-    </form>
+
+
+
+<div class="w-1/2 mx-auto shadow-lg rounded-lg p-8">
+    <h3 class="text-black text-2xl font-bold">Edit language</h3>
+    <div class="mt-4">
+        <form method="post" enctype="multipart/form-data">
+            <?php
+            $lang = get_language($_GET['edit_lang']);
+            ?>
+
+            <label class="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" for="inline-full-name">
+                Name
+            </label>
+            <input type="text" name="name" id="name" class="form-control mt-2" value="<?php echo $lang["name"] ?>" placeholder="Enter lang name">
+            <input type="hidden" name="id" value=" <?php echo $_GET['edit_lang'] ?>"></input>
+            <button type="submit" name="update_lang" class="px-4 py-2 mt-4 rounded-full bg-indigo-600 font-bold text-sm text-white">
+                Update language
+            </button>
+        </form>
+    </div>
+
 </div>
+
+
 
 <?php echo update_lang(); ?>
