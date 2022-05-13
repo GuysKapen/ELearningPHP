d5bd9021836fb05b91f90c0605e47aaf156efbdf
GuysKapen <guyskapen@gmail.com>
2022-05-13 14:52:31 -0800

    Add shortcut link

--- inc/cat_main.php
+++ inc/cat_main.php
@@ -1,4 +1,6 @@
 <ul>
-    <li><a href=""><i class="fa fa-code"></i>Development</a></li>
-    <li><a href=""><i class="fa fa-crop"></i>Design</a></li>
+    <li><a href="courses.php"><i class="fa fa-code"></i>Courses</a></li>
+    <li><a href="course_videos.php"><i class="fa fa-video"></i>Video tutorial</a></li>
+    <li><a href="quizz.php"><i class="fa fa-question"></i>Quiz</a></li>
+    <li><a href="./admin/index.php"><i class="fa fa-lock"></i>Admin</a></li>
 </ul>
